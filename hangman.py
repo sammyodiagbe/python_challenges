e2bc2fe5c59dcbac6ed24be2e13bae8f41b5bc46
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2022-10-11 21:05:45 -0500

    working with python

---
+++ hangman.py
@@ -0,0 +1,50 @@
+from tabnanny import check
+from random_word import RandomWords
+
+word = RandomWords().get_random_word()
+
+word_array = [None] * len(word)
+game_is_done = False
+missed_chance = 0
+
+
+def check_game_status(check_array):
+    for word in check_array:
+        if word is None:
+            return False
+    return True
+def draw_word(word):
+    string = ""
+    for character in word:
+        if character is None:
+            string += '_'
+        else:
+            string += word
+    print(string)
+
+def check_game_status(word, word_array):
+    if word == "".join(word_array):
+        return True
+    if missed_chance >= 4:
+        return True
+    return False
+
+    
+# while the game is not done
+while(not game_is_done):
+    draw_word(word_array)
+    user_selection = input('Enter a letter')
+    # if the user enters a character that is in the word
+    if user_selection in word:
+        for index in range(len(word)):
+            if word[index] == user_selection:
+                word_array[index] = user_selection
+                game_is_done = check_game_status(word_array)
+    else:
+
+        game_is_done = False
+        missed_chance += 1
+
+
+    
+print(word)
