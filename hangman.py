fb0e1810300b7c211f17894b8260bf5ddb747e94
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2022-10-11 21:32:54 -0500

    hangman

--- hangman.py
+++ hangman.py
@@ -3,7 +3,9 @@ from random_word import RandomWords
 
 word = RandomWords().get_random_word()
 
-word_array = [None] * len(word)
+print(word)
+
+word_array = ["_"] * len(word)
 game_is_done = False
 missed_chance = 0
 
@@ -16,20 +18,17 @@ def check_game_status(check_array):
 def draw_word(word):
     string = ""
     for character in word:
-        if character is None:
-            string += '_'
-        else:
-            string += word
+        string += character
     print(string)
 
 def check_game_status(word, word_array):
-    if word == "".join(word_array):
+    if word == ''.join(word_array):
         return True
     if missed_chance >= 4:
         return True
     return False
 
-    
+
 # while the game is not done
 while(not game_is_done):
     draw_word(word_array)
@@ -39,12 +38,11 @@ while(not game_is_done):
         for index in range(len(word)):
             if word[index] == user_selection:
                 word_array[index] = user_selection
-                game_is_done = check_game_status(word_array)
     else:
 
-        game_is_done = False
+       
         missed_chance += 1
-
+    game_is_done = check_game_status(word, word_array)
 
     
 print(word)
